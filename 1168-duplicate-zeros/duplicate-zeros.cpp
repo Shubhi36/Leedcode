6c66c07e7060dff28e302125e6b2a8e1345be4b0
Shubhi Prajapati <prajapatishubhi752@gmail.com>
2026-09-04 15:50:38 +0530

    Time: 14 ms (10.36%) | Memory: 13.8 MB (92.30%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -6,7 +6,7 @@ public:
         i = 0;
         while(i<len)
         {
-            if(arr[i] == 0 && i != len-1)
+            if(arr[i] == 0)
             {
                 j = len-1;
                 k = i;
